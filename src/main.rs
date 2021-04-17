62c4554bceb335a5894f754df85816c702747ec6
Andreas Grois <andi@grois.info>
2021-04-17 14:19:28 +0200

    Change has_thread_local into a dynamic library to fix tls issue

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use libloading::{Library, Symbol};
 use has_thread_local::{set_thread_local, get_thread_local};
 
 fn main() {
-    let lib = Library::new("libshared_library.so").unwrap();
+    let lib = Library::new("target/debug/libshared_library.so").unwrap();
     set_thread_local(10);
     unsafe {
         let func: Symbol<unsafe extern fn() -> u32> = lib.get(b"print_local").unwrap();
